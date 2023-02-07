0da5e437e0b05c816dc3759c39f80fbf20533c12
TusharJain128 <tusharjain128@gmail.com>
2023-02-07 22:19:47 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const app=express()
+
+const mongoose=require("mongoose")
+
+const route=require("./routes/route")
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(
+    "mongodb+srv://TusharJainFunctionup:functionup@tusharjaindb.zxey2fj.mongodb.net/orderManagement",
+    {
+      useNewUrlParser: true,
+    }
+  )
+
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/",route)
+
+app.listen(3000, () => {
+    console.log("Express app running on port " + 3000);
+  });
